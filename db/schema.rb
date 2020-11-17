a50fab04d39d202c7600fce0704de4f5ba5dd316
Blaine <b.atsbeha@gmail.com>
2020-11-17 15:24:17 -0800

    Updated schema - removed Customer

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2020_11_17_034558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "customers", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "merchants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
