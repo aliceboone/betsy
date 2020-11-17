51d6d410a09e001c3609972c5b35015eb76b67ec
Schanen Ryan <schanen.r@gmail.com>
2020-11-16 19:54:56 -0800

    MODELS: Setup for Merchant, Customer & Product.

---
+++ db/migrate/20201117034558_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.integer :inventory
+      t.integer :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
