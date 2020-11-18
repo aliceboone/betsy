4b9be219b895df96dcaf6bb1adb778d5444eef62
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-17 19:32:33 -0800

    created CRUD for Merchants controller and did initial route configuration

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -2,9 +2,12 @@ require "test_helper"
 
 describe MerchantsController do
 
-  it "should get index" do
-    get "/merchants"
-    must_respond_with :success
+  describe "Index" do
+    it "should get index" do
+      get "/merchants"
+      must_respond_with :success
+    end
   end
   
+
 end
