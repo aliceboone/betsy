e1578577471bc79a6b4366255f5b6530f0989d14
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-18 10:36:57 -0800

    added some tests to Merchants controller.

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,13 +1,39 @@
-require "test_helper"
+require 'test_helper'
 
 describe MerchantsController do
 
-  describe "Index" do
-    it "should get index" do
-      get "/merchants"
+  let(:merchant){
+    Merchant.create!(
+      username: 'Jane Do',
+      email: 'jdo@somewhere.com',
+      mailing_address: '1111 3rd Ave NE Seattle WA 98000',
+      credit_last_four: 1111,
+      credit_expire: '12/23' )
+  }
+
+  describe 'Index' do
+    
+    it 'should get index' do
+      get '/merchants'
       must_respond_with :success
     end
   end
-  
+
+  describe 'Show' do
+    it 'can get a valid trip' do
+      get merchant_path trip_path(trip.id)
+
+      must_respond_with :success
+    end
+
+    it 'will redirect for an invalid trip' do
+
+      # Act
+      get trip_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+    end
+  end
 
 end
