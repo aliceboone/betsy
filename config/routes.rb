a1fbb98eedb9f62ba3cbcd6604d0d9472fb61ccd
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-22 00:28:51 -0800

    worked on tests of order and order_items controllers, skipped the ones had issues with sesssions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :homepages, only: [:index]
 
   resources :products do
-    resources :order_items, only: [:create]
+    resources :order_items, only: [:create, :destroy]
     resources :reviews, only: %i[new create]
   end
 
