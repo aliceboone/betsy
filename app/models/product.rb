e27f0d5621579a34656a393fb4faca01f93e2d3e
schanenR <61715125+schanenR@users.noreply.github.com>
2020-11-21 17:51:28 -0800

    Revert "Skr/fixtures"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :merchant
-  belongs_to :category
+  has_and_belongs_to_many :categories
   has_many :reviews
-
-
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {greater_than: 0}
