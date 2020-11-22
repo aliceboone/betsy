909bc604fccc5b6b2fb1be52212664ac2a48ebef
Blaine <b.atsbeha@gmail.com>
2020-11-22 10:59:31 -0800

    relationship tests W.I.P.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe Order do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+
+  # has_many :order_items
+  describe 'relations' do
+    describe 'order_items' do
+      it 'can have many order_items' do
+        # create order
+        # count # order_items in the order
+      end
+    end
+  end
+
+  # custom method add_product(product)
+  # describe 'add_product' do
   # end
-  #
 
 end
