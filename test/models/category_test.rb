31d7036eaf64dd320b1b1ea6eedba4da629c0732
Alice Boone <alicegbonel@gmail.com>
2020-11-25 14:46:42 -0800

    cleaning tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,16 +1,17 @@
 require "test_helper"
 
 describe Category do
-  # describe "validations" do
-  #
-  #   it "category must have a category name" do
-  #     # Arrange
-  #     category = Category.find_by(category_name: 'skincare')
-  #     category.category_name = nil
-  #     # Assert
-  #     expect(category.valid?).must_equal false
-  #     expect(category.errors.messages).must_include category_name
-  #     expect(category.errors.messages[:category_name]).must_equal ["can't be blank"]
-  #   end
-  # end
+
+  describe "validations" do
+
+    it "category must have a category name" do
+      # Arrange
+      category = Category.create!(category_name: 'skincare')
+      category.category_name = nil
+      # Assert
+      expect(category.valid?).must_equal false
+      expect(category.errors.messages).must_include :category_name
+      expect(category.errors.messages[:category_name]).must_equal ["can't be blank"]
+    end
+  end
 end
