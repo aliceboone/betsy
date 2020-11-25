f0817ce3a3a137c4e9a0478f63077fdc889adef5
Alice Boone <alicegbonel@gmail.com>
2020-11-25 01:18:58 -0800

    fixing tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
 
 describe Review do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
   describe "validations" do
     it "review rating must be a number" do
       # Arrange
