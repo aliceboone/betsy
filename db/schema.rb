14cb9f4d15956b6292a0d57b1eb8c3ac5f00169c
Blaine <b.atsbeha@gmail.com>
2020-11-24 23:28:32 -0800

    price_float_migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_182233) do
+ActiveRecord::Schema.define(version: 2020_11_25_065342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_182233) do
     t.string "name"
     t.string "description"
     t.integer "inventory"
-    t.integer "price"
+    t.float "price"
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
